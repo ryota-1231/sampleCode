8abbb04f308f15d12eb8585c367149c9ca873fa5
ryota-1231 <rrrrr.1231@gmail.com>
2020-01-15 22:16:01 +0900

    サインアップ画面実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    added_attrs = [ :nickname, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+  private 
+  def after_sign_in_path_for(resource)
+    messages_path # ログイン後に遷移するpathを設定
+  end
+
 end
