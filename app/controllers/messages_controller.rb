65adc3cd5ad3245abfdca01529db0efca597deed
ryota-1231 <rrrrr.1231@gmail.com>
2020-01-21 18:58:59 +0900

    メッセージ機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,35 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @messages = "message"
+    @messages = Message.all
   end
 
   def delete
     render "users/sign-in"
   end
+
+  def new
+    binding.pry
+  end
+
+  def create
+    binding.pry
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to action: 'index'
+      return
+    else
+      redirect_to action: 'index' 
+      return
+    end
+  end
   
+
+
+  private
+
+  def message_params
+    params.permit(:message)
+  end
+
 end
