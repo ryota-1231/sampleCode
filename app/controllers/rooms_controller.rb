32a8f1b38b4542cf2cf6892f956d331ae5b7b443
ryota-1231 <rrrrr.1231@gmail.com>
2020-01-26 17:13:11 +0900

    双方向通信実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,17 +1,19 @@
 class RoomsController < ApplicationController
   
   def index
-    # 投稿一覧表示に利用
-    @messages = Message.includes(:user).order(:id)
-    # メッセージ投稿に利用
+    @messages = Message.includes(:user).order(:id).last(100)
     @message = current_user.messages.build
   end
-
 
   def show
     # 投稿一覧表示に利用
     @messages = Message.includes(:user).order(:id)
     # メッセージ投稿に利用
     @message = current_user.messages.build
+  end
+
+  def show_additionally
+    last_id = params[:oldest_message_id].to_i - 1    
+    @messages = Message.includes(:user).order(:id).where(id: 1..last_id).last(50)
   end
 end
