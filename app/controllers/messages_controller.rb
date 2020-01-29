d21502cfeb6f6a5e5f7ca89699f9dac76650b861
ryota-1231 <rrrrr.1231@gmail.com>
2020-01-29 18:55:50 +0900

    message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,6 @@ class MessagesController < ApplicationController
     @message = current_user.messages.create!(message_params)
     ActionCable.server.broadcast 'room_channel', message: @message.template
   end
-  
-  def delete
-    render "users/sign-in"
-  end
 
   private
   def message_params
