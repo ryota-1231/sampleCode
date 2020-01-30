54f18862fdfd7ed7e8a00857fd5067d510bde21d
ryota-1231 <rrrrr.1231@gmail.com>
2020-01-30 20:03:05 +0900

    avatar保存実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    added_attrs = [ :nickname, :email, :password, :password_confirmation　]
+    added_attrs = [ :nickname, :email, :password, :password_confirmation, :avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   private 
   def after_sign_in_path_for(resource)
-    messages_path # ログイン後に遷移するpathを設定
+    root_path # ログイン後に遷移するpathを設定
   end
 
   def after_sign_out_path_for(resource)
