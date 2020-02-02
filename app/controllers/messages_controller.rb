8bf27b8bcbc721fbf53f790cee147814fe58302d
ryota-1231 <rrrrr.1231@gmail.com>
2020-02-02 14:28:10 +0900

    image-tag

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,10 @@ class MessagesController < ApplicationController
 
   def create
     @message = current_user.messages.create!(message_params)
+
+    # @messageを「message:」をキーとして「room_channel」に渡す。「.template」はモデルで定義
     ActionCable.server.broadcast 'room_channel', message: @message.template
+
   end
 
   private
